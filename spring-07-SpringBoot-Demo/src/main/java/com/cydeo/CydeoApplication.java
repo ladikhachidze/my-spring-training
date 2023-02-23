3f228ea1ced92b4aa7b61f1f80d86e97dc8267fa
Ladi_Cydeo <ladikhachidze@gmail.com>
2023-02-23 17:01:56 -0500

    spring boot demo 1

--- spring-07-SpringBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
+++ spring-07-SpringBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
@@ -1,13 +1,23 @@
 package com.cydeo;
 
+import com.cydeo.model.Comment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class CydeoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CydeoApplication.class, args);
+
+        Comment comment = new Comment();
+        comment.setAuthor("Vladimer");
+        comment.setText("Spring Framework");
+
+
+       ApplicationContext container =  SpringApplication.run(CydeoApplication.class, args);
+
+
     }
 
 }
